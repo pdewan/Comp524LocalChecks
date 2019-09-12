1a035453c673c0183b423dc8804e0d0fb8cb0107
fzs14 <fzs@cs.unc.edu>
2019-09-12 10:42:54 -0400

    load relation updated

---
+++ src/gradingTools/comp524f19/assignment1/testcases/loadRelation/GorETestedChecker.java
@@ -0,0 +1,14 @@
+package gradingTools.comp524f19.assignment1.testcases.loadRelation;
+
+import main.lisp.parser.terms.SExpression;
+import util.annotations.Explanation;
+@Explanation("Checks if test file has a call to >=")
+public class GorETestedChecker extends AbstractFunctionTestedChecker {
+
+	@Override
+	protected String functionName() {
+		return ">=";
+	}
+
+	
+}
---
+++ src/gradingTools/comp524f19/assignment1/testcases/loadRelation/LorETestedTwoResultsChecker.java
@@ -0,0 +1,14 @@
+package gradingTools.comp524f19.assignment1.testcases.loadRelation;
+
+import main.lisp.parser.terms.SExpression;
+import util.annotations.Explanation;
+@Explanation("Checks if test file has calls to <= with two or more results")
+public class LorETestedTwoResultsChecker extends AbstractFunctionTestedTwoResultsChecker {
+
+	@Override
+	protected String functionName() {
+		return "<=";
+	}
+
+	
+}
