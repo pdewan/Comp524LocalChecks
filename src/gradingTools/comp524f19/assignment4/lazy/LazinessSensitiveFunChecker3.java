8a27020af8b0bf5794934f77453a0e53ceb1c7cf
fzs14 <fzs@cs.unc.edu>
2019-12-01 17:58:57 -0500

    checks for the third part of T18

---
+++ src/gradingTools/comp524f19/assignment4/lazy/LazinessSensitiveFunChecker3.java
@@ -0,0 +1,40 @@
+package gradingTools.comp524f19.assignment4.lazy;
+
+import gradingTools.comp524f19.assignment2.testcases.AbstractLispExpressionResultChecker;
+import util.annotations.Explanation;
+import util.annotations.IsExtra;
+import util.annotations.MaxValue;
+@MaxValue(10)
+@IsExtra(true)
+public class LazinessSensitiveFunChecker3 extends AbstractLispExpressionResultChecker {
+	public static final String[] STUDENT_INPUT = {"(setevalmode \"NORMAL\")", "(car (funcall listMToN 2 5))"};
+	public static final String[] STUDENT_TO_STRING = {
+			"\"NORMAL\"",
+			"2",
+			"3",
+			"4",
+			"5",
+			"2"
+			};
+	public static final String[] GRADER_INPUT = {"(funcall isList (cons 5 6))"};
+	public static final String[] GRADER_TO_STRING = {"NIL"};
+	@Override
+	protected String[] expectedStudentOutput() {
+		return STUDENT_TO_STRING;
+	}
+	@Override
+	protected String[] expectedGraderOutput() {
+		return GRADER_TO_STRING;
+	}
+	@Override
+	protected String[] studentInputLines() {
+		return STUDENT_INPUT;
+	}
+	@Override
+	protected String[] graderInputLines() {
+		// TODO Auto-generated method stub
+		return GRADER_INPUT;
+	}
+
+	
+}
---
+++ src/gradingTools/comp524f19/assignment4/lazy/LazinessSensitiveFunSuite.java
@@ -0,0 +1,23 @@
+package gradingTools.comp524f19.assignment4.lazy;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import gradingTools.comp524f19.assignment2.testcases.LispFunctionCheckers.LoadLispChecker;
+import gradingTools.comp524f19.assignment2.testcases.LispFunctionCheckers.TestLispFileProvided;
+import util.annotations.IsExtra;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	LazinessSensitiveFunChecker3.class,
+	LazinessSensitiveFunChecker4.class
+
+	
+})
+	
+@IsExtra(true)
+public class LazinessSensitiveFunSuite {
+	
+}
+
