7bec656065f3e3b97b9ee5efd6466ba435d84fc0
dewan <dewan@cs.unc.edu>
2021-01-22 09:59:29 -0500

    Reusing shared greeting registry

--- src/gradingTools/comp524f20/assignment0_1/GreetingClassRegistry.java
+++ src/gradingTools/comp524f20/assignment0_1/GreetingClassRegistry.java
@@ -1,5 +1,5 @@
 package gradingTools.comp524f20.assignment0_1;
 
-public interface GreetingClassRegistry {
-	public Class<?> getGreetingMain();
+public interface GreetingClassRegistry extends gradingTools.shared.testcases.greeting.GreetingClassRegistry {
+//	public Class<?> getGreetingMain();
 }
