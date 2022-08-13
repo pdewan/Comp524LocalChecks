c67a1adbe46dfa94e7aa2404fe9e21e8839624d2
0c1e468e4e4b1d39812fd2c8c5a0cba81828a5682eced2a6ca1e50afe43e42, 13ae3ed6fe76d459c9c66fe38ff187593561a1f24d34cb22e06148c77e4cc02b(2627dc3fbf149f58a61f9f94d28baed5bb59cf68f64b4ff611d3bc54a0cb81da)/ <ajwortas@gmail.com>
2022-08-13 19:45:01 -0400

    F22 A0

---
+++ src/gradingTools/comp524f22/assignment0/F22Assignment0Suite.java
@@ -0,0 +1,39 @@
+package gradingTools.comp524f22.assignment0;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import grader.basics.config.BasicExecutionSpecificationSelector;
+import grader.basics.junit.BasicJUnitUtils;
+import gradingTools.comp524f21.assignment0_1.A01Style;
+import gradingTools.shared.testcases.greeting.GreetingClassRegistryProvided;
+import gradingTools.shared.testcases.greeting.GreetingMainProvided;
+import gradingTools.shared.testcases.greeting.GreetingRun;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	GreetingClassRegistryProvided.class,
+	GreetingMainProvided.class,
+	GreetingRun.class,
+	A01Style.class
+	
+})
+
+public class F22Assignment0Suite {
+
+	public static void main (String[] args) {
+		try {
+			BasicJUnitUtils.interactiveTest(F22Assignment0Suite.class);
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	static {
+		BasicExecutionSpecificationSelector.getBasicExecutionSpecification().
+		setCheckStyleConfiguration("unc_checks_524_A0_1.xml");
+	}
+	
+}
---
+++ src/gradingTools/comp524f22/assignment0/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author dewan
+ *
+ */
+package gradingTools.comp524f22.assignment0;
